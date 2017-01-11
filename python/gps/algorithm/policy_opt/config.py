62cd2600b124319bb4bad309230b40e1e55b76cd
Soroush Nasiriany <snasiriany@gmail.com>
2017-01-11 11:24:39 -0800

    new policy files

--- python/gps/algorithm/policy_opt/config.py
+++ python/gps/algorithm/policy_opt/config.py
@@ -38,11 +38,10 @@ POLICY_OPT_CAFFE = {
 POLICY_OPT_CAFFE.update(GENERIC_CONFIG)
 
 
-checkpoint_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                               '..', 'policy_opt/tf_checkpoint/policy_checkpoint.ckpt'))
 POLICY_OPT_TF = {
     # Other hyperparameters.
-    'checkpoint_prefix': checkpoint_path
+    'copy_param_scope': 'conv_params',
+    'fc_only_iterations': 0,
 }
 
 POLICY_OPT_TF.update(GENERIC_CONFIG)
